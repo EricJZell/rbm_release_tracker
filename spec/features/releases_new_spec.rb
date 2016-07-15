d338e37138c41685bb6d7354ede84523121e2eb6
Eric Zell <ezell@rbmtechnologies.com>
2016-07-15 19:02:44 -0400

    yep

--- spec/features/releases_new_spec.rb
+++ spec/features/releases_new_spec.rb
@@ -13,15 +13,15 @@ feature 'user creates a new release', %{
 } do
 
   let!(:apps) { [App.create(name: "VMM"), App.create(name: "Vodafone")]}
-  let!(:clients) { [FactoryGirl.create(:client, app: apps[0]),
-                    FactoryGirl.create(:client, app: apps[0])] }
+  let!(:clients) { [FactoryGirl.create(:client, app: apps[0], name: "ATT"),
+                    FactoryGirl.create(:client, app: apps[0]), name: "Cricket"] }
 
   scenario 'user creates a valid new release' do
     visit app_path(apps[0])
     click_link "Add a new release"
     fill_in "Branch Name", with: "rel_15_5_1_rc"
     fill_in "Tag Name", with: "v15.5.1"
-    check "att"
+    check "ATT"
     click_button "Create Release"
     expect(page).to have_content("New Release Created Successfully")
   end
@@ -29,7 +29,7 @@ feature 'user creates a new release', %{
   scenario 'user fills out form with invalid inputs' do
     visit app_path(apps[0])
     click_link "Add a new release"
-    check "att"
+    check "ATT"
     click_button "Save"
     expect(page).to have_content("Branch Name can't be blank")
   end
