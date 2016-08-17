7dd14a514bbc146235c2a165e103daf9fd878f55
Eric Zell <ezell@rbmtechnologies.com>
2016-08-17 10:37:31 -0400

    Add db seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Release.delete_all
+Client.delete_all
+App.delete_all
+
+
+apps = App.create([{ name: "VMM" }, { name: "Magellan" }, { name: "CPC" },
+    { name: "RBM Validator" }])
+
+vmm = App.find_by_name("VMM")
+
+clients = Client.create([
+  { name: "AT&T", abbreviated_name: "att", app: vmm },
+  { name: "Best Buy", abbreviated_name: "bby", app: vmm },
+  { name: "Capital One", abbreviated_name: "cap", app: vmm },
+  { name: "Cox", abbreviated_name: "cox", app: vmm },
+  { name: "Cricket", abbreviated_name: "cricket", app: vmm },
+  { name: "O2", abbreviated_name: "o2", app: vmm },
+  { name: "Optus", abbreviated_name: "optus", app: vmm },
+  { name: "T-mobile", abbreviated_name: "tmobile", app: vmm },
+  { name: "UBC", abbreviated_name: "ubc", app: vmm },
+  { name: "Verizon Wireless", abbreviated_name: "vzw", app: vmm },
+  { name: "Sunglass Hut AUS/NZL", abbreviated_name: "sgh-anz", app: vmm }
+  ])
